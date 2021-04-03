72c94b3740701d6eb7a7a3e72e835e800d119702
AyoBakre <ayobakre@gmail.com>
2021-04-03 20:12:12 +0100

    proper dates and times

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 
 app = Flask(__name__)
@@ -15,6 +16,7 @@ login = LoginManager(app)
 mail = Mail(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import routes, models, errors
 
